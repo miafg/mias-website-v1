45026b14bdd232577543e4afc92605bc25312a58
mgalli200 <miaf@sas.upenn.edu>
2016-07-13 13:39:22 -0600

    changed layout of website

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,29 @@
 /**
  * Created by mgalli200 on 6/10/16.
  */
-var app = angular.module("myApp", []);
-var main = function() {
+var app = angular.module('myApp', ['ngRoute']);
 
-}
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/photos', {
+            templateUrl: 'photos.html',
+            controller: 'photosCtrl'
+        })
+        .when('/aboutme', {
+            templateUrl: 'aboutMe.html',
+            controller: 'aboutmeCtrl'
+        })
+        .when('/interests', {
+            templateUrl: 'interests.html',
+            controller: 'interestsCtrl'
+        })
+        .when('/experience', {
+            templateUrl: 'experience.html',
+            controller: 'experienceCtrl'
+        })
+        .otherwise({redirectTo: '/'});
+});
--- app/js/controller.js
+++ app/js/controller.js
@@ -1,7 +1,7 @@
 
 app.controller('myCtrl', ['$scope', function($scope) {
 	$scope.title = "Welcome to Mia's Website";
-	$scope.homepageImg = "../img/maroon_bells.jpg";
+	
 	$scope.goToAboutMe = function() {
 		$state.go("aboutMe.html");
 
