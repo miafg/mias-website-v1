361f3cdc032f45cb642039bb7dc143d2b4a0b7ef
mgalli200 <miaf@sas.upenn.edu>
2016-07-19 13:34:36 -0600

    added buttons and form to contact

--- app/js/contact.js
+++ app/js/contact.js
@@ -1,6 +1,7 @@
 /**
  * Created by mgalli200 on 7/13/16.
  */
-app.controller('contactCtrl', function($scope) {
-    
+app.controller('contactCtrl', function($scope, $window) {
+    $scope.photo = "app/img/hanging_lake.jpg";
+    $scope.email = "mailto:miaf@sas.upenn.edu";
 });
