cf552b6b4a27b90cf42a38df12c1bb5df3410735
mgalli200 <miaf@sas.upenn.edu>
2016-07-19 15:46:47 -0600

    Formatted experience page

--- app/js/experience.js
+++ app/js/experience.js
@@ -2,5 +2,7 @@
  * Created by mgalli200 on 7/13/16.
  */
 app.controller('experienceCtrl', function($scope) {
-    
+    $scope.aoeImg = "app/img/aoe.jpeg";
+    $scope.comcastImg = "app/img/comcast.png";
+    $scope.upennImg = "app/img/upenn.gif";
 });
